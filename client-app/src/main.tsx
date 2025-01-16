8c39f7749ef9bc1afe6bfe130f62961d2dacd01b
Toaha Muhammad <toaham04@gmail.com>
2025-01-16 00:13:57 -0500

    End of section 7

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -4,9 +4,13 @@ import App from './app/layout/App.tsx'
 import 'semantic-ui-css/semantic.min.css'
 
 import './app/layout/styles.css'
+import { store, StoreContext } from './app/stores/store.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <StoreContext.Provider value={store}>
+      <App />
+    </StoreContext.Provider>
+
   </StrictMode>,
 )
