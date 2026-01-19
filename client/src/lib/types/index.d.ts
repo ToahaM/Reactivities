7c8fca716b3e72b23fa328dd737779715410241e
Toaha Bin Muhammad <toaham04@gmail.com>
2026-01-18 20:30:15 -0500

    End of section 13

--- client/src/lib/types/index.d.ts
+++ client/src/lib/types/index.d.ts
@@ -10,7 +10,15 @@ type Activity = {
     latitude: number
     longitude: number
 }
-export type LocationIQSuggestion = {
+
+type User = {
+  id: string
+  email: string
+  displayName: string
+  imageUrl?: string
+}
+
+type LocationIQSuggestion = {
     place_id: string
     osm_id: string
     osm_type: string
@@ -26,7 +34,7 @@ export type LocationIQSuggestion = {
     address: LocationIQAddress
   }
   
-  export type LocationIQAddress = {
+  type LocationIQAddress = {
     name: string
     house_number: string
     road: string
