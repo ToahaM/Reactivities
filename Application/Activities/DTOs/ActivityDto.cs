79068e313ecd3bb8f2e2ff78446a956341a605c5
Toaha Bin Muhammad <toaham04@gmail.com>
2026-01-22 11:37:11 -0500

    End of section 14

--- Application/Activities/DTOs/ActivityDto.cs
+++ Application/Activities/DTOs/ActivityDto.cs
@@ -3,12 +3,17 @@ using Application.Profiles.DTOs;
 
 namespace Application.Activities.DTOs;
 
-public class BaseActivityDto
+public class ActivityDto
 {
+    public string Id {get; set;} = "";
     public string Title { get; set; } = "";
     public DateTime Date { get; set; }
     public string Description { get; set; } = string.Empty;
     public string Category { get; set; } = "";
+
+    public bool IsCancelled { get; set; }
+    public required string HostDisplayName { get; set; }
+    public required string HostId { get; set; }
     
 
     // location props
@@ -18,5 +23,8 @@ public class BaseActivityDto
     public double Latitude { get; set; }
     public double Longitude { get; set; }
 
+    
+    // navigation properties
 
+    public ICollection<UserProfile> Attendees { get; set; } = [];
 }
