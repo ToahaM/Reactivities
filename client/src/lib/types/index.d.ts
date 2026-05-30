5ceb044a9a042e3e63f6340b69c92ef4f7acbaac
Toaha Bin Muhammad <toaham04@gmail.com>
2026-05-30 18:11:46 -0400

    End of section 19

--- client/src/lib/types/index.d.ts
+++ client/src/lib/types/index.d.ts
@@ -1,20 +1,20 @@
 type Activity = {
-    id: string
-    title: string
-    date: Date
-    description: string
-    category: string
-    isCancelled: boolean
-    city: string
-    venue: string
-    latitude: number
-    longitude: number
-    attendees: Profile[]
-    isGoing: boolean
-    isHost: boolean
-    hostId: string
-    hostDisplayName: string
-    hostImageUrl: string
+  id: string
+  title: string
+  date: Date
+  description: string
+  category: string
+  isCancelled: boolean
+  city: string
+  venue: string
+  latitude: number
+  longitude: number
+  attendees: Profile[]
+  isGoing: boolean
+  isHost: boolean
+  hostId: string
+  hostDisplayName: string
+  hostImageUrl: string
 }
 
 type Profile = {
@@ -36,34 +36,43 @@ type User = {
   imageUrl?: string
 }
 
+type ChatComment = {
+  id: string
+  createdAt: Date
+  body: string
+  userId: string
+  displayName: string
+  imageUrl?: string
+}
+
 type LocationIQSuggestion = {
-    place_id: string
-    osm_id: string
-    osm_type: string
-    licence: string
-    lat: string
-    lon: string
-    boundingbox: string[]
-    class: string
-    type: string
-    display_name: string
-    display_place: string
-    display_address: string
-    address: LocationIQAddress
-  }
-  
-  type LocationIQAddress = {
-    name: string
-    house_number: string
-    road: string
-    suburb?: string
-    town?: string
-    village?: string
-    city?: string
-    county: string
-    state: string
-    postcode: string
-    country: string
-    country_code: string
-    neighbourhood?: string
-  }
+  place_id: string
+  osm_id: string
+  osm_type: string
+  licence: string
+  lat: string
+  lon: string
+  boundingbox: string[]
+  class: string
+  type: string
+  display_name: string
+  display_place: string
+  display_address: string
+  address: LocationIQAddress
+}
+
+type LocationIQAddress = {
+  name: string
+  house_number: string
+  road: string
+  suburb?: string
+  town?: string
+  village?: string
+  city?: string
+  county: string
+  state: string
+  postcode: string
+  country: string
+  country_code: string
+  neighbourhood?: string
+}
